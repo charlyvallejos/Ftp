636240506903b9604e0f9abbcb253b1061f1adad
charly <charlyvallejos22@gmail.com>
2019-09-23 16:58:38 -0300

    Merge origin/master
    Conflicts:
    	nbproject/private/private.xml

--- clases/config.php
+++ clases/config.php
@@ -14,9 +14,9 @@ class Config
     {
         $this->tipo = "mysql";
         $this->host = "localhost";
-        $this->dbname = "saporitiweb";//"id10739155_saporitiweb";
-        $this->user = "root";// "id10739155_admin";
-        $this->pw = "";//deamon";
+        $this->dbname = "id10739155_saporitiweb";
+        $this->user = "id10739155_admin";
+        $this->pw = "deamon";
         $this->opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
     }
 }
